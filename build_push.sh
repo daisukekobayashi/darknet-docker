b185195d0b500cac64a02cd99e1d14fa3651982b
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2020-09-09 20:11:04 +0900

    update build script

--- build_push.sh
+++ build_push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for var in cpu gpu gpu-cc53 gpu-cc60 gpu-cc61 gpu-cc62 gpu-cc70 gpu-cc72 gpu-cc75 \
-          cpu-cv gpu-cv gpu-cv-cc53 gpu-cv-cc60 gpu-cv-cc61 gpu-cv-cc62 gpu-cv-cc70 gpu-cv-cc72 gpu-cv-cc75
+for var in cpu cpu-noopt gpu gpu-cc53 gpu-cc60 gpu-cc61 gpu-cc62 gpu-cc70 gpu-cc72 gpu-cc75 \
+          cpu-cv cpu-noopt-cv gpu-cv gpu-cv-cc53 gpu-cv-cc60 gpu-cv-cc61 gpu-cv-cc62 gpu-cv-cc70 gpu-cv-cc72 gpu-cv-cc75
 do
   DOCKER_REPO="daisukekobayashi/darknet"
-  SOURCE_BRANCH="darknet_yolo_v3"
+  SOURCE_BRANCH="darknet_yolo_v4_pre"
   SOURCE_COMMIT=`git ls-remote https://github.com/AlexeyAB/darknet.git darknet_yolo_v3 | awk '{ print $1 }'`
   DOCKER_TAG=$SOURCE_BRANCH-$var
   VAR=$var
@@ -16,7 +16,8 @@ do
   echo $VAR
 
 if [[ "$DOCKER_TAG" == *cv* ]]; then
-  if [ "$DOCKER_TAG" = "cpu-cv" ]; then
+  if [[ "$DOCKER_TAG" = *cpu-cv || "$DOCKER_TAG" = *cpu-noopt-cv ]]; then
+    echo "building cpu-cv or cpu-noopt-cv"
     docker build --build-arg BASE_IMAGE="ubuntu:18.04" \
       --build-arg CONFIG=$VAR \
       --build-arg ADDITIONAL_PACKAGES="libopencv-dev" \
@@ -24,6 +25,7 @@ if [[ "$DOCKER_TAG" == *cv* ]]; then
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
       -t $DOCKER_REPO:$DOCKER_TAG .
   else
+    echo "building gpu-cv"
     docker build \
       --build-arg CONFIG=$VAR \
       --build-arg ADDITIONAL_PACKAGES="libopencv-dev" \
@@ -32,13 +34,15 @@ if [[ "$DOCKER_TAG" == *cv* ]]; then
       -t $DOCKER_REPO:$DOCKER_TAG .
   fi
  else
-  if [ "$DOCKER_TAG" = "cpu" ]; then
+  if [[ "$DOCKER_TAG" = *cpu || "$DOCKER_TAG" = *cpu-noopt ]]; then
+    echo "building cpu or cpu-noopt"
     docker build --build-arg BASE_IMAGE="ubuntu:18.04" \
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
       -t $DOCKER_REPO:$DOCKER_TAG .
   else
+    echo "building gpu"
     docker build \
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
