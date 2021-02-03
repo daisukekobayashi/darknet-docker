1bebf459bfb3d2f0a9281e384b04243dd22d4957
Daisuke Kobayashi <daisuke@daisukekobayashi.com>
2021-02-03 23:31:57 +0900

    reduce image size

--- build_push.sh
+++ build_push.sh
@@ -5,7 +5,7 @@ for var in cpu cpu-noopt gpu gpu-cc53 gpu-cc60 gpu-cc61 gpu-cc62 gpu-cc70 gpu-cc
 do
   DOCKER_REPO="daisukekobayashi/darknet"
   SOURCE_BRANCH="darknet_yolo_v4_pre"
-  SOURCE_COMMIT=`git ls-remote https://github.com/AlexeyAB/darknet.git darknet_yolo_v3 | awk '{ print $1 }'`
+  SOURCE_COMMIT=`git ls-remote https://github.com/AlexeyAB/darknet.git ${SOURCE_BRANCH} | awk '{ print $1 }'`
   DOCKER_TAG=$SOURCE_BRANCH-$var
   VAR=$var
 
@@ -18,36 +18,34 @@ do
 if [[ "$DOCKER_TAG" == *cv* ]]; then
   if [[ "$DOCKER_TAG" = *cpu-cv || "$DOCKER_TAG" = *cpu-noopt-cv ]]; then
     echo "building cpu-cv or cpu-noopt-cv"
-    docker build --build-arg BASE_IMAGE="ubuntu:18.04" \
+    docker build \
       --build-arg CONFIG=$VAR \
-      --build-arg ADDITIONAL_PACKAGES="libopencv-dev" \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG .
+      -t $DOCKER_REPO:$DOCKER_TAG -f cpu-cv/Dockerfile .
   else
     echo "building gpu-cv"
     docker build \
       --build-arg CONFIG=$VAR \
-      --build-arg ADDITIONAL_PACKAGES="libopencv-dev" \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG .
+      -t $DOCKER_REPO:$DOCKER_TAG -f gpu-cv/Dockerfile .
   fi
  else
   if [[ "$DOCKER_TAG" = *cpu || "$DOCKER_TAG" = *cpu-noopt ]]; then
     echo "building cpu or cpu-noopt"
-    docker build --build-arg BASE_IMAGE="ubuntu:18.04" \
+    docker build \
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG .
+      -t $DOCKER_REPO:$DOCKER_TAG -f cpu/Dockerfile .
   else
     echo "building gpu"
     docker build \
       --build-arg CONFIG=$VAR \
       --build-arg SOURCE_BRANCH=$SOURCE_BRANCH \
       --build-arg SOURCE_COMMIT=$SOURCE_COMMIT \
-      -t $DOCKER_REPO:$DOCKER_TAG .
+      -t $DOCKER_REPO:$DOCKER_TAG -f gpu/Dockerfile .
   fi
 fi
 
